598b831089778eef609da0f66675c6abf5b106a1
equal-l2 <eng.equall2@gmail.com>
2018-09-24 12:21:10 +0900

    config.fish: don't execute tmux

--- config.fish
+++ config.fish
@@ -40,8 +40,3 @@ function fish_prompt --description "My own prompt!"
 
   echo -e '\n\e[7m[ '(prompt_pwd)" : $statstr $job]\e[m\n$suffix "
 end
-
-if status is-interactive
-and not set -q TMUX
-    tmux
-end
