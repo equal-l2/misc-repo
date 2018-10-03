d7b83994533d41a7cd47e3bb51d9b9b66b032a70
equal-l2 <eng.equall2@gmail.com>
2018-10-03 14:38:55 +0900

    local_macos.fish: update

--- local_macos.fish
+++ local_macos.fish
@@ -1,10 +1,43 @@
+set -x fish_emoji_width 2
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
-set -x RUSTFLAGS '-C linker=rust-lld -Z linker-flavor=ld64.lld'
 set -x RUST_SRC_PATH ~/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
 
-set -x JAVA_HOME `/usr/libexec/java_home`
+set -x JAVA_HOME (/usr/libexec/java_home)
 set -x PATH ~/clogsys ~/bin /usr/local/sbin /usr/local/opt/texinfo/bin ~/go/bin /opt/metasploit-framework/bin/ /usr/local/bin $PATH
+
+function daily-update
+  brew upgrade --fetch-HEAD
+
+  brew cask upgrade --greedy
+
+  rustup update
+  cargo install-update -a
+
+  pip3 list --outdated --format=columns
+  pip list --outdated --format=columns
+  npm up -g
+end
+
+function cc-san
+  cc \
+  -g \
+  -fsanitize=address,integer,nullability,undefined \
+  -fno-omit-frame-pointer \
+  -fno-optimize-sibling-calls \
+  -fsanitize-address-use-after-scope \
+  $argv
+end
+
+function c++-san
+  c++ \
+  -g \
+  -fsanitize=address,integer,nullability,undefined \
+  -fno-omit-frame-pointer \
+  -fno-optimize-sibling-calls \
+  -fsanitize-address-use-after-scope \
+  $argv
+end
 
 # opam configuration
 source ~/.opam/opam-init/init.fish
