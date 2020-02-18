713326c8314003c7408014dbc229f3cc2a5d74b7
equal-l2 <eng.equall2@gmail.com>
2020-02-18 22:01:00 +0900

    template.cpp: add utilities

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <cmath>
 #include <iomanip>
 #include <iostream>
@@ -7,6 +8,27 @@
 #include <vector>
 
 using namespace std;
+
+// read to vector
+// for example M_RTV(as, int, n) expands to the below:
+// ```
+// vector<int> as(n);
+// for(int lcnt = 0; lcnt < n; lcnt++) cin >> as[lcnt];
+// ```
+#define M_RTV(ident, type, cnt) \
+    do {\
+        vector<type> ident(cnt);\
+        for(int lcnt = 0; lcnt < cnt; lcnt++) cin >> ident[lcnt];\
+    } while(false)
+
+// shorthand for range
+#define M_RG(ident) ident.begin(), ident.end()
+
+// https://qiita.com/EqualL2/items/b4683db7ab4e90545bb2
+int ctoi(const char c){
+    if('0' <= c && c <= '9') return (c-'0');
+    return -1;
+}
 
 int main() {
 }
